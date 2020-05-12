9dda22148515a01cfe67fba2017ae2d6a41afe6a
Gabriel <cgpaiva@icloud.com>
2020-05-12 14:12:03 -0300

    init

--- src/main/java/com/olheiros/dateOlheiros/DateOlheirosController.java
+++ src/main/java/com/olheiros/dateOlheiros/DateOlheirosController.java
@@ -3,6 +3,7 @@ package com.olheiros.dateOlheiros;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import java.util.Calendar;
 @Controller
 public class DateOlheirosController {
 
-    @GetMapping
+    @GetMapping("/trueDate")
     @ResponseBody
     public ResponseEntity<DateModel> currentMonth() {
 
